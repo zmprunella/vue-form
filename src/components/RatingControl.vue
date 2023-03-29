d2dc2637ec062ff98524b328670a808cad51ce63
ZMP <zane.prunella@gmail.com>
2023-03-29 11:15:00 -0700

    vue-form

---
+++ src/components/RatingControl.vue
@@ -0,0 +1,62 @@
+<template>
+  <ul>
+    <li :class="{ active: modelValue === 'poor' }">
+      <button type="button" @click="activate('poor')">Poor</button>
+    </li>
+    <li :class="{ active: modelValue === 'average' }">
+      <button type="button" @click="activate('average')">Average</button>
+    </li>
+    <li :class="{ active: modelValue === 'great' }">
+      <button type="button" @click="activate('great')">Great</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['modelValue'],
+  emits: ['update:modelValue'],
+  //   data() {
+  //     return {
+  //       activeOption: this.modelValue,
+  //     };
+  //   },
+
+  methods: {
+    activate(option) {
+      this.$emit('update:modelValue', option);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.active {
+  border-color: pink;
+}
+.active button {
+  color: pink;
+}
+
+ul {
+  list-style: none;
+  margin: 0.5 rem 0;
+  padding: 0;
+  display: flex;
+}
+
+li {
+  margin: 0 1rem;
+  border: 1px solid grey;
+  padding: 1rem;
+  justify-content: center;
+  align-items: center;
+}
+
+button {
+  font: inherit;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+}
+</style>
